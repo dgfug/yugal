21c6f0bf6505ccf69d3881a192d5fef0f127b351
Paurush Sinha <76876045+sinhapaurush@users.noreply.github.com>
2021-06-09 18:35:43 +0530

    Delete lib/tminc-extras directory

--- lib/tminc-extras/header.php
+++
@@ -1,8 +0,0 @@
-<?php
-    function toast($message){
-        ?>
-        <script>toast("<?php echo str_replace('"', '\"', $message)?>")</script>
-        <?php
-    }
-?>
-<script src="lib/tminc-extras/javascript.js"></script>
