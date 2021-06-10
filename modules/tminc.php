d71dbe42e83695b1f93cbfbede46be4754127d76
Paurush Sinha <76876045+sinhapaurush@users.noreply.github.com>
2021-06-10 20:53:58 +0530

    Update tminc.php

--- modules/tminc.php
+++ modules/tminc.php
@@ -38,7 +38,6 @@ function ob_html_compress($buf){
                 }
         ?>
             <meta charset="UTF-8">
-            <link rel="stylesheet" href="design/tminc.css">
             <meta name="viewport" content="width=device-width, initial-scale=1.0"><link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
                 <script src="modules/mod.js"></script>
             <?php 
